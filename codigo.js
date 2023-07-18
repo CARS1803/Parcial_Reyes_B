a24f807e67f7830cfeaff9f0ad758033a0d0e01c
Reyes <Carlosreyes1803@gmail.com>
2023-07-18 08:48:50 -0600

    creacion de las constantes y una alerta por si sedeja vacio el input de consulta

---
+++ codigo.js
@@ -0,0 +1,13 @@
+//Primer se crean las constantes que se van a utulizar
+const formulario = document.querySelector('form')
+const tabla = document.querySelector('table')
+
+const consultarPais = async (e) => {
+    e.preventDefault();
+    let nombrePais = formulario.pais.value;
+    if(nombrePais === ''){
+        alert("Debe ingresar el nombre de un pais");
+        return;
+    }
+    
+}
