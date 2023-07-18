75eb1f26da68b29550d2fb41e41786f84a1a01f5
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-07-18 10:37:14 -0600

    detalles

--- codigo.js
+++ codigo.js
@@ -1,11 +1,114 @@
 const API_URL = "https://restcountries.com/v3.1/";
 
+const xhr = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if (this.readyState === 4 && this.status === 200) {
+        const data = JSON.parse(this.response);
+        console.log(data);
+        const pais = data[0];
+        console.log(pais.flag);
+        console.log(pais.name.common);
+        console.log(pais.population);
+        console.log(pais.capital[0]);
+        
+        document.getElementById('banderaPais').src = pais.flags.png;
+        document.getElementById('nombrePais').innerText = pais.name.common;
+        document.getElementById('poblacionPais').innerText = pais.population;
+        document.getElementById('capitalPais').innerText = pais.capital[0];
+        document.getElementById('resultado').innerText = 'Fue Encontrado';
+        document.querySelector('#tabla1').style.display = 'block';
+    }
+}
+
+xhr.addEventListener("load", onRequestHandler);
+
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let nombrePais = formulario.pais.value;
+    if (nombrePais === '') {
+        alert("Debe ingresar el nombre de un país");
+        return;
+    }
+
+    const url = `${API_URL}/name/${nombrePais}`;
+    const config = {
+        method: 'GET'
+    };
+
+    document.getElementById('resultado').innerText = 'Buscando el archivo que desea...';
+
+    try {
+        const respuesta = await fetch(url, config);
+        if (respuesta.status === 200) {
+            const data = await respuesta.json();
+            const pais = data[0];
+            console.log(pais.flag);
+            console.log(pais.name.common);
+            console.log(pais.population);
+            console.log(pais.capital[0]);
+            
+            document.getElementById('banderaPais').src = pais.flags.png;
+            document.getElementById('nombrePais').innerText = pais.name.common;
+            document.getElementById('poblacionPais').innerText = pais.population;
+            document.getElementById('capitalPais').innerText = pais.capital[0];
+            document.getElementById('resultado').innerText = 'Fue Encontrado';
+            document.querySelector('#tabla1').style.display = 'block';
+        } else {
+            document.getElementById('resultado').innerText = 'No se encontró';
+            document.querySelector('#tabla1').style.display = 'none';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+const formulario2 = document.querySelector('#form');
+formulario2.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let nombreRegion = formulario2.region.value;
+
+    const url = `${API_URL}/region/${nombreRegion}`;
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        if (respuesta.status === 200) {
+            const data = await respuesta.json();
+            const pais = data[0];
+            console.log(pais.region);
+            console.log(pais.continents[0]);
+            console.log(pais.subregion);
+            
+            document.getElementById('regionPais').innerText = pais.region;
+            document.getElementById('continentePais').innerText = pais.continents[0];
+            document.getElementById('subregionPais').innerText = pais.subregion;
+            document.getElementById('resultado').innerText = 'Fue Encontrado';
+            document.querySelector('#tabla2').style.display = 'block';
+        } else {
+            document.getElementById('resultado').innerText = 'No se encontró';
+            document.querySelector('#tabla2').style.display = 'none';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+const APIi_URL = "https://restcountries.com/v3.1/";
+
 // const HTMLResponse = document.querySelector('#app')
 // const ul = document.createElement('ul')
 
 fetch(`${API_URL}/all`)
 
-const xhr = new XMLHttpRequest();
+const xhrr = new XMLHttpRequest();
 
 function onRequestHandler() {
     if (this.readyState === 4 && this.status === 200) {
@@ -20,51 +123,5 @@ function onRequestHandler() {
 }
 
 xhr.addEventListener("load", onRequestHandler);
-xhr.open("GET", `${API_URL}/all`);
+xhr.open("GET", `${APIi_URL}/all`);
 xhr.send();
-
-
-//Primer se crean las constantes que se van a utulizar
-const formulario = document.querySelector('form')
-const tabla = document.querySelector('table')
-
-const consultarPais = async (e) => {
-    e.preventDefault();
-    let nombrePais = formulario.pais.value;
-    if(nombrePais === ''){
-        alert("Debe ingresar el nombre de un pais");
-        return;
-    }
-
-    const url=`https://restcountries.com/v3.1/name/${nombrePais}`;
-    const config = {
-        method: 'GET'
-    };
-
-    document.getElementById('resultado').innerText = 'Buscando el archivo que desea...'
-
-    try {
-        const respuesta = await fetch(url, config);
-        if(respuesta.status) {
-            const data = await respuesta.json();
-            const pais = data[0];
-            console.log(pais.flag);
-            console.log(pais.name.common);
-            console.log(pais.population);
-            console.log(pais.timezones);
-            
-            document.getElementById('banderaPais').src = pais.flags.png;
-            document.getElementById('nombrePais').innerText = pais.name.common;
-            document.getElementById('poblacionPais').innerText = pais.population;
-            document.getElementById('capitalPais').innerText = pais.timezones[0];
-            document.getElementById('resultado').innerText = 'Fue Encontrado';
-            tabla.style.display = '';
-        } else {
-            document.getElementById('resultado').innerText = 'No se encontro';
-        }
-    }catch (error) {
-        console.log(error);
-    }
-}
-
-formulario.addEventListener('submit', consultarPais)
