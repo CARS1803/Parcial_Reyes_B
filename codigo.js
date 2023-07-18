2db07b0d5f0d8722cdff6f39f693a768647219f3
Reyes <Carlosreyes1803@gmail.com>
2023-07-18 09:17:53 -0600

    Se agrego un div en html y en java scrip se genero el codigo para que  mande a llamar toda la informacion por la consola y asi poder reviar los nodos

--- codigo.js
+++ codigo.js
@@ -1,3 +1,29 @@
+const API_URL = "https://restcountries.com/v3.1/";
+
+// const HTMLResponse = document.querySelector('#app')
+// const ul = document.createElement('ul')
+
+fetch(`${API_URL}/all`)
+
+const xhr = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if (this.readyState === 4 && this.status === 200) {
+        // console.log(this.response);  // ESTE COMANDO ES PARA QUE MANDE A LLAMAR TODA LA INFORMACION EN FORMATO TEXTO DE LOS OBJETOS
+        const data = JSON.parse(this.response);
+        console.log(data);   //ACA PODREMOS VER LA MISMA INFORMACION PERO MEJOR ESTRUCUTRADA POR NODOS PARA LLAMAR A LA INFORMACION DE UNA MANERA MAS SENCIALLA
+        // const HTMLResponse = document.querySelector('#app');
+
+        // const tpl = data.map((user) => `<li>${user.name} ${user.email}</li>`)
+        // HTMLResponse.innerHTML = `<ul>${tpl}</ul>`
+    }
+}
+
+xhr.addEventListener("load", onRequestHandler);
+xhr.open("GET", `${API_URL}/all`);
+xhr.send();
+
+
 //Primer se crean las constantes que se van a utulizar
 const formulario = document.querySelector('form')
 const tabla = document.querySelector('table')
@@ -22,17 +48,23 @@ const consultarPais = async (e) => {
         if(respuesta.status) {
             const data = await respuesta.json();
             const pais = data[0];
+            console.log(pais.flag);
             console.log(pais.name.common);
             console.log(pais.population);
             console.log(pais.timezones);
-            console.log(pais.flag);
-
+            
+            document.getElementById('banderaPais').src = pais.flags.png;
             document.getElementById('nombrePais').innerText = pais.name.common;
-            document.getElementById().innerText = pais.population;
-            document.getElementById().innerText = pais.timezones[0];
-            document.getElementById().innerText = pais.flags.png;
-            document.getElementById().innerText = 'Fue Encontrado';
+            document.getElementById('poblacionPais').innerText = pais.population;
+            document.getElementById('capitalPais').innerText = pais.timezones[0];
+            document.getElementById('resultado').innerText = 'Fue Encontrado';
             tabla.style.display = '';
+        } else {
+            document.getElementById('resultado').innerText = 'No se encontro';
         }
+    }catch (error) {
+        console.log(error);
     }
 }
+
+formulario.addEventListener('submit', consultarPais)
