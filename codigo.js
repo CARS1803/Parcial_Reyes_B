cd5b9c27efa85ccc8961ee386bdd5eab07fb1fd4
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-07-18 11:44:54 -0600

    Finalizacion

--- codigo.js
+++ codigo.js
@@ -32,7 +32,7 @@ formulario.addEventListener('submit', async (e) => {
             console.log(pais.capital[0]);
             
             document.getElementById('banderaPais').src = pais.flags.png;
-            document.getElementById('regionPais').innerText = pais.region; // Corrección en el ID
+            document.getElementById('regionPais').innerText = pais.region; 
             document.getElementById('nombrePais').innerText = pais.name.common;
             document.getElementById('poblacionPais').innerText = pais.population;
             document.getElementById('capitalPais').innerText = pais.capital[0];
@@ -46,6 +46,9 @@ formulario.addEventListener('submit', async (e) => {
         console.log(error);
     }
 });
+
+
+
 
 const formulario2 = document.querySelector('#form');
 formulario2.addEventListener('submit', async (e) => {
@@ -64,11 +67,14 @@ formulario2.addEventListener('submit', async (e) => {
             const pais = data[0];
             console.log(pais.region);
             console.log(pais.continents[0]);
+            console.log(pais.area);
             console.log(pais.subregion);
+
             
             document.getElementById('regionPai').innerText = pais.region;
-            document.getElementById('continentePais').innerText = pais.continents[0];
+            document.getElementById('areaPais').innerText = pais.area;
             document.getElementById('subregionPais').innerText = pais.subregion;
+            document.getElementById('continentePais').innerText = pais.continents[0];
             document.getElementById('resultado').innerText = 'Fue Encontrado';
             document.querySelector('#tabla2').style.display = 'block';
         } else {
@@ -83,7 +89,51 @@ formulario2.addEventListener('submit', async (e) => {
 
 
 
+const formulario3 = document.querySelector('#form3');
+formulario3.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let nombreSubregion = formulario3.subregion.value;
 
+    const url = `${API_URL}/subregion/${nombreSubregion}`;
+    const config = {
+        method: 'GET'
+    };
+
+    try {
+        const respuesta = await fetch(url, config);
+        if (respuesta.status === 200) {
+            const data = await respuesta.json();
+            const pais = data[0];
+            console.log(pais.population);
+            console.log(pais.capitalInfo.latlng);
+            console.log(pais.coatOfArms.png);
+
+            
+            
+
+            
+            document.getElementById('poblacionPais').innerText = pais.population;
+            document.getElementById('ubicacionPais').innerText = pais.capitalInfo.latlng;
+            document.getElementById('escudo').src = pais.coatOfArms.png;
+
+
+
+            document.getElementById('resultado').innerText = 'Fue Encontrado';
+            document.querySelector('#tabla3').style.display = 'block';
+        } else {
+            document.getElementById('resultado').innerText = 'No se encontró';
+            document.querySelector('#tabla3').style.display = 'none';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+//PARA REVISAR EN LA CONSOLA LOS NODOS A CONSULTAR
 const APIi_URL = "https://restcountries.com/v3.1/";
 
 // const HTMLResponse = document.querySelector('#app')
